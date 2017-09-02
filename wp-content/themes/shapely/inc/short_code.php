bb970e309a97e324c3f2bbf447efcf1c80169ed2
Hsu <github.1up@0sg.net>
2017-09-03 07:07:16 +0800

    save

--- wp-content/themes/shapely/inc/short_code.php
+++ wp-content/themes/shapely/inc/short_code.php
@@ -367,13 +367,14 @@ function contact_shortcode($args, $content) {
 						setMarkers(map, eval('addr_info_'+locationId));
 					}
 				});
-
-      	$.get('http://app.knackmap.com/geoip.php', function(city_code) {
-      		var $tab = $("[data-city-code='" + city_code + "']")
-      		if ($tab.length > 0) {
-      			$tab.trigger('click')
-      		}
-      	})
+				setTimeout(function() {
+	      	$.get('http://app.knackmap.com/geoip.php', function(city_code) {
+	      		var $tab = $("[data-city-code='" + city_code + "']")
+	      		if ($tab.length > 0) {
+	      			$tab.trigger('click')
+	      		}
+	      	})
+	      }, 1000)
 			});
 
 			function initMap(lat, lng, arr_info_json) {
